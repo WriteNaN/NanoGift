6c9ae0f88bb46fa4c884124d7fd2a08fcfa5f5f4
Write Int <151211283+WriteNaN@users.noreply.github.com>
2024-03-18 19:52:40 +0530

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,17 @@
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+#!/bin/bash
+
+if [[ $(docker ps -q) ]]; then
+    docker stop $(docker ps -q)
+    echo "Running containers stopped."
+else
+    echo "No running containers to stop."
+fi
+
+if [[ $(docker ps -aq) ]]; then
+    docker rm $(docker ps -aq)
+    echo "Stopped containers removed."
+else
+    echo "No stopped containers to remove."
+fi
+
+echo "Container cleanup complete."
