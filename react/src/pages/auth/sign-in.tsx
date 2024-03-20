dbf9897238f4fcdfc130174e1818c5b9b961111b
WriteInt <154680855+WriteInt@users.noreply.github.com>
2024-03-20 11:54:44 +0000

    :moon: Dark theme

--- react/src/pages/auth/sign-in.tsx
+++ react/src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet";
 import { SignIn as Login } from "@clerk/clerk-react";
+import {dark} from "@clerk/themes";
 
 import "../../styles/glow.css";
 import "../../styles/index.css";
@@ -15,7 +16,9 @@ export default function SignIn() {
 
             <div className={`fixed glow-blue min-h-screen items-center justify-center inset-0`}>
                 <div className="absolute inset-0 flex items-center justify-center">
-                    <Login />
+                    <Login appearance={{
+                        baseTheme: dark
+                    }} />
                 </div>
             </div>
 
